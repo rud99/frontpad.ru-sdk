e78487477e22204d55fd16b587c55e1840fa9352
Asan Abdurakhmanov <asan11@outlook.com>
2020-12-27 19:54:09 +0200

    update

---
+++ README.md
@@ -0,0 +1,72 @@
+# Sdk for frontpad.ru
+
+### Create order
+
+```php
+$frontpad = new \Kolirt\Frontpad\Frontpad('secret');
+
+$order = new \Kolirt\Frontpad\Order;
+$order->setName('Oleh');
+$order->setPhone('phonenumber');
+$order->setMail('qwe@qwe.qwe');
+$order->setStreet('street name');
+$order->setHome('19');
+$order->setPod('1');
+$order->setEt('9');
+$order->setApart('920');
+$order->setDescr('Comment');
+$order->setSaleAmount(100);
+$order->addHookStatus(10);
+$order->setAffiliate(20);
+$order->setPoint(30);
+$order->setPerson(4);
+$order->setDateTime('2016-08-15 15:30:00');
+
+
+$order->adTag(10);
+$order->adTag(11);
+$order->adTag(12);
+
+
+$product1 = new \Kolirt\Frontpad\Product();
+$product1->setIndex($order->getNewProductIndex());
+$product1->setId(100);
+$product1->setQuantity(2);
+$product1->setPrice(100);
+$order->addProduct($product1);
+
+$product2 = new \Kolirt\Frontpad\Product();
+$product2->setIndex($order->getNewProductIndex());
+$product2->setModIndex($product1->getIndex());
+$product2->setId(101);
+$product2->setQuantity(2);
+$product2->setPrice(30);
+$order->addProduct($product2);
+
+
+$frontpad->newOrder($order);
+```
+
+### Get client
+
+```php
+$frontpad = new \Kolirt\Frontpad\Frontpad('secret');
+
+$frontpad->getClient('client_phone');
+```
+
+### Get certificate
+
+```php
+$frontpad = new \Kolirt\Frontpad\Frontpad('secret');
+
+$frontpad->getCertificate('certificate');
+```
+
+### Get products
+
+```php
+$frontpad = new \Kolirt\Frontpad\Frontpad('secret');
+
+$frontpad->getProducts();
+```
---
+++ src/Frontpad.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Kolirt\Frontpad;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+
+class Frontpad
+{
+
+    private $client;
+    private $api_endpoint = 'https://app.frontpad.ru/api/index.php';
+    private $secret;
+
+    public function __construct($secret)
+    {
+        $this->secret = $secret;
+        $this->client = new Client([
+            'base_uri' => $this->api_endpoint,
+            'headers'  => [
+                'Accept' => 'application/json'
+            ]
+        ]);
+    }
+
+    /**
+     * Метод предназначен для передачи заказа из интернет-магазина или приложения.
+     *
+     * @param Order $order
+     * @return object
+     */
+    public function newOrder(Order $order)
+    {
+        return $this->call(function () use ($order) {
+            $form_params = $order->render();
+            $form_params['secret'] = $this->secret;
+
+            $request = $this->client->post('?new_order', [
+                'form_params' => $form_params
+            ]);
+
+            return $this->prepareResponse($request);
+        });
+    }
+
+    /**
+     * Метод предназначен для получения информации о клиенте, форма проверки может быть установлена
+     * в личном кабинете интернет-магазина.
+     * Запрещается циклический поиск клиентов.
+     *
+     * @param $client_phone
+     * @return object
+     */
+    public function getClient($client_phone)
+    {
+        return $this->call(function () use ($client_phone) {
+            $request = $this->client->post('?get_client', [
+                'form_params' => [
+                    'secret'       => $this->secret,
+                    'client_phone' => $client_phone
+                ]
+            ]);
+
+            return $this->prepareResponse($request);
+        });
+    }
+
+    /**
+     * Метод предназначен для проверки сертификата, форма проверки может
+     * быть установлена в личном кабинете интернет-магазина.
+     * Запрещается циклическая проверка сертификата.
+     *
+     * @param $certificate
+     * @return object
+     */
+    public function getCertificate($certificate)
+    {
+        return $this->call(function () use ($certificate) {
+            $request = $this->client->post('?get_certificate', [
+                'form_params' => [
+                    'secret'      => $this->secret,
+                    'certificate' => $certificate
+                ]
+            ]);
+
+            return $this->prepareResponse($request);
+        });
+    }
+
+    /**
+     * Метод предназначен для получения списка товаров и синхронизации их с интернет-магазином.
+     * Выгружаются только товары, имеющие артикул.
+     *
+     * @return object
+     */
+    public function getProducts()
+    {
+        return $this->call(function () {
+            $request = $this->client->post('?get_products', [
+                'form_params' => [
+                    'secret' => $this->secret,
+                ]
+            ]);
+
+            return $this->prepareResponse($request);
+        });
+    }
+
+    private function prepareResponse(Response $response)
+    {
+        $data = $response->getBody()->getContents();
+        $data = is_json($data) ? json_decode($data) : $data;
+
+        return (object)[
+            'ok'          => $data->result === 'success',
+            'status_code' => $response->getStatusCode(),
+            'response '   => $response,
+            'data'        => $data
+        ];
+    }
+
+    private function call($function)
+    {
+        try {
+            return $function();
+        } catch (\Exception $exception) {
+            $response = $exception->getResponse();
+            return $this->prepareResponse($response);
+        }
+    }
+
+}
